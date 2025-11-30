7b9d5f6bf21f30f2ef6abd6e3ae0edbff46390fd
Ritwik Shanker <ritwikshanker@yahoo.in>
2025-11-30 15:51:49 +0100

    chore: Bump version to 2.0
    This commit increments the `versionCode` to 2 and the `versionName` to "2.0".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.deutschdreamers.wordimpostor"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
